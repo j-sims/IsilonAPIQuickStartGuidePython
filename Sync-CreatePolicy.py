e12ca5238e698a7997372b8aa37ae9337f7f3918
Andrew Collins <andrew.collins@dell.com>
2022-05-18 10:19:00 -0500

    added 'Sync-ListPolicies.py', 'Sync-CreatePolicy.py', and 'Sync-DeletePolicy.py'

---
+++ Sync-CreatePolicy.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # Supresses the self signed cert warning
+
+CLUSTERIP = '172.16.10.10'
+PORT = 8080
+USER = 'root'
+PASS = 'a'
+
+uri = "https://%s:%s" % (CLUSTERIP, PORT)
+papi = uri + '/platform'
+headers = {'Content-Type': 'application/json'}
+data = json.dumps({'username': USER, 'password': PASS, 'services': ['platform']})
+
+# uri of the cluster used in the referer header
+uri = f"https://{CLUSTERIP}:{PORT}"
+# url of Papi used for all further calls to Papi
+papi = uri + '/platform'
+# Set header as content will provided in json format
+headers = {'Content-Type': 'application/json'}
+# Create json dictionary for auth
+data = json.dumps({'username': USER, 'password': PASS, 'services': ['platform']})
+# create a session object to hold cookies
+session = requests.Session()
+# Establish session using auth credentials
+response = session.post(uri + "/session/1/session", data=data, headers=headers, verify=False)
+if 200 <= response.status_code < 299:
+    # Set headers for CSRF protection. Without these two headers all further calls with be "auth denied"
+    session.headers['referer'] = uri
+    session.headers['X-CSRF-Token'] = session.cookies.get('isicsrf')
+    print("Authorization Successful")
+else:
+    print("Authorization Failed")
+    print(response.content)
+
+data = {'source_root_path': '/ifs/data/sourcefolder1', 'target_path': '/ifs/data/targetfolder1', 'action': 'sync',
+        'name': 'policy1', 'target_host': '172.16.10.10'}
+
+endpoint = '/7/sync/policies'
+response = session.post(papi + endpoint, data=json.dumps(data), headers=headers, verify=False)
+
+if 200 <= response.status_code < 299:
+    print("Policy Creation Successful")
+else:
+    print("Policy Creation Failed")
